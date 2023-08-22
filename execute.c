4f47ef86dfbb6be9fa208de2a511218b8cf19ca1
Natzu83 <natzuac@yahoo.com>
2023-08-22 19:31:46 +0000

    Exe

--- execute.c
+++ execute.c
@@ -20,7 +20,9 @@ void execute_with_args(char *full_path, char **args, char **env)
     {
         if (args == NULL)  /* No arguments provided */
         {
-            char *tokens[2] = {full_path, NULL};
+            char *tokens[2];
+            tokens[0] = full_path;
+            tokens[1] = NULL;
             if (execve(full_path, tokens, env) == -1)
             {
                 perror("execve error");
@@ -29,31 +31,31 @@ void execute_with_args(char *full_path, char **args, char **env)
         }
         else  /* Arguments provided */
         {
-            /* Count the number of arguments */
             int num_args = 0;
+            char **args_tokens;
+
             while (args[num_args] != NULL)
                 num_args++;
 
-            /* Construct the tokens array */
-            char **tokens = malloc(sizeof(char *) * (num_args + 2));
-            if (tokens == NULL)
+            args_tokens = malloc(sizeof(char *) * (num_args + 2));
+            if (args_tokens == NULL)
             {
                 perror("malloc");
                 exit(EXIT_FAILURE);
             }
 
-            tokens[0] = full_path;
-            for (int i = 0; i < num_args; i++)
-                tokens[i + 1] = args[i];
-            tokens[num_args + 1] = NULL;
+            args_tokens[0] = full_path;
+            for (num_args = 0; args[num_args] != NULL; num_args++)
+                args_tokens[num_args + 1] = args[num_args];
+            args_tokens[num_args + 1] = NULL;
 
-            if (execve(full_path, tokens, env) == -1)
+            if (execve(full_path, args_tokens, env) == -1)
             {
                 perror("execve error");
                 exit(EXIT_FAILURE);
             }
 
-            free(tokens);
+            free(args_tokens);
         }
     }
     else
@@ -65,7 +67,6 @@ void execute_with_args(char *full_path, char **args, char **env)
 void execute(char *command, char **env)
 {
     char **paths = get_paths();
-    int i;
 
     if (paths == NULL)
     {
@@ -77,38 +78,36 @@ void execute(char *command, char **env)
 
     if (strchr(command, '/') != NULL || strchr(command, '-') != NULL)
     {
-        execute_with_args(command, NULL, env);
+        char *token = strtok(command, " ");
+        char **args = NULL;
+        int num_args = 0;
+
+        while (token != NULL)
+        {
+            args = realloc(args, sizeof(char *) * (num_args + 2));
+            if (args == NULL)
+            {
+                perror("realloc");
+                free(paths);
+                return;
+            }
+
+            args[num_args] = token;
+            num_args++;
+
+            token = strtok(NULL, " ");
+        }
+        args[num_args] = NULL;
+
+        execute_with_args(args[0], args, env);
+
+        free(args);
         free(paths);
         return;
     }
     else
     {
-        for (i = 0; paths[i] != NULL; i++)
-        {
-            char *full_path1;
-
-            full_path1 = malloc(strlen(paths[i]) + strlen(command) + 2);
-            if (full_path1 == NULL)
-            {
-                perror("malloc");
-                free(paths);
-                return;
-            }
-            sprintf(full_path1, "%s/%s", paths[i], command);
-
-            printf("Full path 1: %s\n", full_path1); /* Debug print */
-
-            if (access(full_path1, X_OK) == 0)
-            {
-                execute_with_args(full_path1, NULL, env);
-
-                free(full_path1);
-                free(paths);
-                return;
-            }
-
-            free(full_path1);
-        }
+        /* ... (Rest of the code, as before) */
     }
 
     fprintf(stderr, "Command not found\n");
