1ff303e2e264648009a1c331a52bf2f603947333
Natzu83 <natzuac@yahoo.com>
2023-08-22 18:54:03 +0000

    Exe

--- execute.c
+++ execute.c
@@ -1,10 +1,10 @@
 #include "simple_shell.h"
 
 /**
- * execute - Function to execute a command using fork and execve
+ * execute_with_args - Function to execute a command with arguments using fork and execve
  *
- * @command: The command to execute
- *
+ * @full_path: The full path to the executable
+ * @args: The arguments to the command (including the command itself)
  * @env: The environment variables
  */
 void execute_with_args(char *full_path, char **args, char **env)
@@ -18,10 +18,42 @@ void execute_with_args(char *full_path, char **args, char **env)
 
     if (child_pid == 0)
     {
-        if (execve(full_path, args, env) == -1)
+        if (args == NULL)  /* No arguments provided */
         {
-            perror("execve error");
-            exit(EXIT_FAILURE);
+            char *tokens[2] = {full_path, NULL};
+            if (execve(full_path, tokens, env) == -1)
+            {
+                perror("execve error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else  /* Arguments provided */
+        {
+            /* Count the number of arguments */
+            int num_args = 0;
+            while (args[num_args] != NULL)
+                num_args++;
+
+            /* Construct the tokens array */
+            char **tokens = malloc(sizeof(char *) * (num_args + 2));
+            if (tokens == NULL)
+            {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+            }
+
+            tokens[0] = full_path;
+            for (int i = 0; i < num_args; i++)
+                tokens[i + 1] = args[i];
+            tokens[num_args + 1] = NULL;
+
+            if (execve(full_path, tokens, env) == -1)
+            {
+                perror("execve error");
+                exit(EXIT_FAILURE);
+            }
+
+            free(tokens);
         }
     }
     else
@@ -41,6 +73,8 @@ void execute(char *command, char **env)
         return;
     }
 
+    printf("Executing command: %s\n", command); /* Debug print */
+
     if (strchr(command, '/') != NULL || strchr(command, '-') != NULL)
     {
         execute_with_args(command, NULL, env);
@@ -52,7 +86,7 @@ void execute(char *command, char **env)
         for (i = 0; paths[i] != NULL; i++)
         {
             char *full_path1;
-            
+
             full_path1 = malloc(strlen(paths[i]) + strlen(command) + 2);
             if (full_path1 == NULL)
             {
@@ -61,6 +95,8 @@ void execute(char *command, char **env)
                 return;
             }
             sprintf(full_path1, "%s/%s", paths[i], command);
+
+            printf("Full path 1: %s\n", full_path1); /* Debug print */
 
             if (access(full_path1, X_OK) == 0)
             {
